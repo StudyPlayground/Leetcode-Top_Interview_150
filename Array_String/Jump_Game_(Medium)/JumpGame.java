b0c4f2486306d8a54a426ab5c153c01e6abc18e4
Il Young Hwang <ilyoung6016@gmail.com>
2024-04-18 12:51:17 +0900

    Update JumpGame.java
    solve again

--- Array_String/Jump_Game_(Medium)/JumpGame.java
+++ Array_String/Jump_Game_(Medium)/JumpGame.java
@@ -1,33 +1,45 @@
 class Solution {
+
+    static boolean[] visited;
+
     public boolean canJump(int[] nums) {
-        // 현재까지 이동할 수 있는 최대 인덱스를 저장하는 변수
-        int maxJump = 0;
-        // 배열의 길이를 저장하는 변수
-        int length = nums.length;
-        
-        // 배열의 길이가 1인 경우 마지막 인덱스에 도달했으므로 true 를 반환
-        if(length == 1) {
+
+        visited = new boolean[nums.length];
+
+        if(nums.length == 1) {
             return true;
         }
+
+        return canReachEnd(nums, 0);
         
-        // 배열을 순회하면서 이동 가능한 최대 인덱스를 갱신
-        for(int i = 0; i < nums.length; i++) {
-            // 현재 위치가 현재까지 이동할 수 있는 최대 인덱스를 넘어선 경우
-            if(i > maxJump) {
-                // 더 이상 이동할 수 없으므로 false 반환
-                return false;
-            }
+    }
 
-            // 현재 위치에서 이동할 수 있는 최대 인덱스 갱신
-            maxJump = Math.max(i + nums[i], maxJump);
+    public boolean canReachEnd(int[] nums, int position) {
 
-            // 마지막 인덱스에 도달할 수 있는 경우 true 반환
-            if(maxJump >= length) {
-                return true;
-            }
+        if(position >= nums.length-1) {
+            return true;
         }
 
-        // 배열을 모두 순회 했지만, 인덱스에 도달할 수 없는 경우 false 반환
+        if(visited[position]) {
+            return false;
+        }
+
+        if(nums[position] == 0) {
+            return false;
+        }
+
+        visited[position] = true;
+
+        for(int i = nums[position]; i >= 1; i--) {
+            
+            if(canReachEnd(nums, position + i)) {
+                return true;
+            } 
+
+            if(i == 1) {
+                return false;
+            }
+        }
         return false;
     }
 }
