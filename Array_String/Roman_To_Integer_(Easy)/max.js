22308b4955e66355be0f0b7f4582040ed2882fbc
pirateTurtle <75886763+wogha95@users.noreply.github.com>
2024-05-07 21:20:00 +0900

    Roman_To_Integer_(Easy)

--- Array_String/Roman_To_Integer_(Easy)/max.js
+++ Array_String/Roman_To_Integer_(Easy)/max.js
@@ -2,26 +2,35 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    let h = {
-        'I' : 1,
-        'V' : 5,
-        'X' : 10,
-        'L' : 50,
-        'C' : 100,
-        'D' : 500,
-        'M' : 1000
+var romanToInt = function (s) {
+    const ROMAN = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1,
     }
+    const ROMAN_LIST = Object.entries(ROMAN);
+    let sIndex = 0;
     let result = 0;
-    for(let i =0 ; i < s.length  ; i++ ){
-        if(h[s[i]] < h[s[i+1]]){
-            temp = h[s[i+1]] - h[s[i]];
-            result += temp
-            i++
+
+    for (let index = 0; index < ROMAN_LIST.length && sIndex < s.length;) {
+        const [key, value] = ROMAN_LIST[index];
+        if (s.slice(sIndex, sIndex + key.length) === key) {
+            result += value;
+            sIndex += key.length;
+        } else {
+            index += 1;
         }
-        else{
-            result += h[s[i]];
-        }
-    } 
+    }
+
     return result;
 };
