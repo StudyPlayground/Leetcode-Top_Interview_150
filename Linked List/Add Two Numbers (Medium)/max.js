0033c8c6c3081e9fdb15e3e70e52f68d4c79e585
wogha95 <wogha95@naver.com>
2024-05-30 21:06:10 +0900

    Add Two Numbers (Medium)

---
+++ Linked List/Add Two Numbers (Medium)/max.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+  const result = new ListNode();
+  let currentResult = result;
+  let roundValue = 0;
+
+  while (l1 !== null && l2 !== null) {
+    connectNewNode(l1.val + l2.val + roundValue, () => {
+      l1 = l1.next;
+      l2 = l2.next;
+    });
+  }
+
+  while (l1 !== null) {
+    connectNewNode(l1.val + roundValue, () => {
+      l1 = l1.next;
+    });
+  }
+
+  while (l2 !== null) {
+    connectNewNode(l2.val + roundValue, () => {
+      l2 = l2.next;
+    });
+  }
+
+  if (roundValue > 0) {
+    const newNode = new ListNode(roundValue);
+
+    currentResult.next = newNode;
+  }
+
+  return result.next;
+
+  function connectNewNode(value, goNextNode) {
+    const newNode = new ListNode(value % 10);
+    roundValue = Math.floor(value / 10);
+
+    currentResult.next = newNode;
+    currentResult = currentResult.next;
+
+    goNextNode();
+  }
+};
